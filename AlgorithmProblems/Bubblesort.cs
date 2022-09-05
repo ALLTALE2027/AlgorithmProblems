f9b1b24d86d34316bc17ee295ef2da702150be7d
ALLTALE2027 <86816300+ALLTALE2027@users.noreply.github.com>
2022-09-05 23:59:56 +0530

    [Darshan]: add bubble sort for sorting string array

---
+++ AlgorithmProblems/Bubblesort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProblems
+{
+    internal class Bubblesort
+    {
+
+        public static void IntegerArraySorting(int[] array)
+        {
+            for (var i = 0; i < array.Length; i++)
+            for (var j = 1; j < array.Length-i; j++)
+                if (array[j] < array[j - 1])
+                {
+                    (array[j], array[j - 1]) = (array[j - 1], array[j]);
+                }
+        }
+
+        public static void StringArraySorting(string[] array)
+        {
+            for (var i = 0; i < array.Length; i++)
+            for (var j = 1; j < array.Length-i; j++)
+                if (array[j].ToLower().CompareTo(array[j - 1].ToLower()) < 0)
+                    (array[j], array[j - 1]) = (array[j - 1], array[j]);
+        }
+    }
+}
---
+++ AlgorithmProblems/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace AlgorithmProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Algorithm Problems!!!");
+            Console.WriteLine("1.Bubble sort");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    string[] name = { "Spiderman", "Batman", "Superman","Ironman","Antman"};
+                    Bubblesort.StringArraySorting(name);
+                    Console.WriteLine(string.Join(" ", name));
+                    break;
+                default: Console.WriteLine("Enter correct choice");
+                    break;
+
+            }
+           
+        }
+    }
+}
