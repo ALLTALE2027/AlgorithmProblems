cc6f954c54dcf9c018e4b3e35c369c1e184ce9b8
ALLTALE2027 <86816300+ALLTALE2027@users.noreply.github.com>
2022-09-06 00:14:57 +0530

    [Darshan]: add insertion sort to sort string array

---
+++ AlgorithmProblems/Insertionsort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProblems
+{
+    internal class Insertionsort
+    {
+        public static void Insertion( string[] array)
+        {
+            for (var i = 1; i < array.Length; i++)
+            {
+                var current = array[i];
+
+                var j = i - 1;
+                while (j >= 0 && array[j].ToLower().CompareTo(current.ToLower()) > 0)
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j + 1] = current;
+            }
+            
+        }
+    }
+}
--- AlgorithmProblems/Program.cs
+++ AlgorithmProblems/Program.cs
@@ -6,6 +6,7 @@
         {
             Console.WriteLine("Algorithm Problems!!!");
             Console.WriteLine("1.Bubble sort");
+            Console.WriteLine("2.Insertion sort");
             int choice = int.Parse(Console.ReadLine());
 
             switch (choice)
@@ -14,6 +15,11 @@
                     string[] name = { "Spiderman", "Batman", "Superman","Ironman","Antman"};
                     Bubblesort.StringArraySorting(name);
                     Console.WriteLine(string.Join(" ", name));
+                    break;
+                case 2:
+                    string[] names = { "Spiderman", "Batman", "Superman", "Ironman", "Antman" };
+                    Insertionsort.Insertion(names);
+                    Console.WriteLine(string.Join(" ", names));
                     break;
                 default: Console.WriteLine("Enter correct choice");
                     break;
